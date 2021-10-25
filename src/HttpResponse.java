97a97b87e38ff7e79cebc4b4dfd84758fc9725b5
ly40 <ly40@st-andrews.ac.uk>
2021-10-25 18:45:25 +0100

    init

--- src/HttpResponse.java
+++ src/HttpResponse.java
@@ -2,11 +2,13 @@
  * Copyright 2021 Damon Yu
  */
 
+import java.io.Serializable;
+
 /**
  * @author damonyu
  * @version 1.0
  * @since 24/10/2021
  */
-public class HttpResponse {
+public class HttpResponse implements Serializable {
 
 }
